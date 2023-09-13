a7e4b05dc9ed9ad3659fbee15c62faa090871553
Marcelo <Marcelo@DESKTOP-4P0OFGT>
2023-09-12 21:55:30 -0400

    Add project files.

---
+++ VentasNEXUS/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VentasNEXUS.Models
+{
+    public class Client
+    { 
+       /* public int ID { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public int nit { get; set; }*/
+        
+
+    }
+}
