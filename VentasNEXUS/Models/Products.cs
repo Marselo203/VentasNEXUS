eac3ea4754961a8b33c55fd83450563b16034b4b
Marcelo203 <brayanmvy2003@gmail.com>
2023-09-13 14:18:47 -0400

    GA

---
+++ VentasNEXUS/Models/Products.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VentasNEXUS.Models
+{
+    public class Products
+    {
+        public int id_P { get; set; }
+        public string name { get; set; }
+        public float price { get; set;}
+        public string description { get; set; }
+        public int stock { get; set; }
+
+
+
+    }
+}
---
+++ VentasNEXUS/Models/Sales.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VentasNEXUS.Models
+{
+    public class Sales
+    {
+        public int id_s { get; set; }
+        public Client Client_s{get; set;}
+        public Products product_s { get; set; }
+    }
+}
