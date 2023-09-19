11567f5370dbb104eef63582b2b3e56b6a2a8c96
esteban7856 <129599542+Andres66666@users.noreply.github.com>
2023-09-19 10:34:46 -0400

    filtracion de datos en cliente y producto

--- VentasNEXUS/Pages/Client/Index.cshtml.cs
+++ VentasNEXUS/Pages/Client/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using VentasNEXUS.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using VentasNEXUS.Models;
 
 namespace VentasNEXUS.Pages.Client
@@ -20,10 +21,25 @@ namespace VentasNEXUS.Pages.Client
         }
 
         public IList<Models.Client> Client { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public SelectList? Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
             Client = await _context.Client.ToListAsync();
+            var clientee = from m in _context.Client
+                           select m;
+            var cli = from m in _context.Client
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                cli = cli.Where(s => s.Name.Contains(SearchString));
+            }
+
+            Client = await cli.ToListAsync();
         }
     }
 }
